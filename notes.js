22f7c1e9996de9e4c309231aa6097afd34cfed1b
Hayden-Neilson <61288409+Hayden-Neilson@users.noreply.github.com>
2020-06-18 10:01:12 -0600

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,2 +1,17 @@
 // 6/17/2020 - overview of react and similar languages
 // 6/18/2020 - var, let, const, functions and arrow functions
+// var myName = 'Max'
+// console.log(myName)
+
+// // arrow function 
+
+//  const printMyName = (name) => {
+//   console.log(name)
+// }
+// printMyName('max')
+
+class Person {
+  constructor(){
+    this.name = "max"
+  }
+}
