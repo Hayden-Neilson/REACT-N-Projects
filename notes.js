546bcbacde1f536711fd1e11595e3da973959007
Hayden-Neilson <haydenneilson5@gmail.com>
2020-06-22 22:02:37 -0600

    how to use a arrow function

--- notes.js
+++ notes.js
@@ -1,2 +1,3 @@
 // 6/17/2020 - overview of react and similar languages
 // 6/18/2020 - var, let, const, functions and arrow functions
+//  6/22/2020 - how the arrow function can be used as a argument.
