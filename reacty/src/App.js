e95631753be549d80391ff3ed2dd66ea2ae2d3e7
Hayden-Neilson <haydenneilson5@gmail.com>
2020-07-18 20:07:23 -0600

    notes on state and props

--- reacty/src/App.js
+++ reacty/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi I'm a react app</h1>
+      <button>Switch Name</button>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>This is really working</p>
+      <Person name="max" age="17" />
+      <Person name="george" age="54" />
+      <Person name="betty" age="37" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
