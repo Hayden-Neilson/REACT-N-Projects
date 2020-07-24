fcacff6d7e20e2942690114197e21653c5a7ea6b
Hayden-Neilson <haydenneilson5@gmail.com>
2020-07-23 21:39:16 -0600

    EVENT listeners

--- reacty/src/App.js
+++ reacty/src/App.js
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "george", age: 54 },
+      { name: "betty", age: 37 }
+
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("was clicked")
+  };
+
+}
   return (
     <div className="App">
       <h1>Hi I'm a react app</h1>
-      <button>Switch Name</button>
+      <button onClick>Switch Name</button>
       <img src={logo} className="App-logo" alt="logo" />
       <p>This is really working</p>
       <Person name="max" age="17" />
       <Person name="george" age="54" />
       <Person name="betty" age="37" />
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
     </div>
   );
-}
+};
 
 export default App;
