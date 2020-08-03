d9ac4c4f224a1e5205324f5a4fc6ca9584c018b0
Hayden-Neilson <haydenneilson5@gmail.com>
2020-08-03 16:27:17 -0600

    Practice what i learned

--- reacty/src/Person/person.js
+++ reacty/src/Person/person.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
+import "./Person.scss";
 const Person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p>
         I'm {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
